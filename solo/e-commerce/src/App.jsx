fe6471ae0c8adf2b54f98903a0eceff25a7e832b
thaivhm <hoangminhthaiv@gmail.com>
2025-04-14 22:57:12 +0700

    add navbar

--- solo/e-commerce/src/App.jsx
+++ solo/e-commerce/src/App.jsx
@@ -1,6 +1,12 @@
 //
 import { Routes, Route } from "react-router-dom";
 import { PromoBar } from "./components/common/PromoBar";
+import { Navbar } from "./components/layout/Navbar";
 export default function App() {
-  return <PromoBar />;
+  return (
+    <>
+      <PromoBar />
+      <Navbar />
+    </>
+  );
 }
---
+++ solo/e-commerce/src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+//
+import { NavLink } from "react-router-dom";
+export function Navbar() {
+  return (
+    <nav className="flex justify-center py-6">
+      <div className="w-4/5 flex items-center">
+        <div className="cursor-pointer px-5 py-2">
+          <span className="font-bold text-6xl ">Exclusive</span>
+        </div>
+        <div className="flex justify-center grow">
+          <NavLink to="../../App.jsx">
+            <span className="text-3xl p-4">Home</span>
+          </NavLink>
+          <NavLink to="./">
+            <span className="text-3xl p-4">About</span>
+          </NavLink>
+          <NavLink to="./">
+            <span className="text-3xl p-4">Contact</span>
+          </NavLink>
+          <NavLink to="./">
+            <span className="text-3xl p-4">Sign Up</span>
+          </NavLink>
+        </div>
+        <div>
+          <p className="text-3xl">Search form</p>
+        </div>
+      </div>
+    </nav>
+  );
+}
